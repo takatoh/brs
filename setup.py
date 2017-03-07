7827223b919ea6b9dc412af52c94f0df9ad40d51
takatoh <takatoh.m@gmail.com>
2017-03-07 20:50:34 +0900

    setup.py: New.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+setuptools.setup(
+    name='brs',
+    version='0.1.1',
+    description='Post book data to Bruschetta web app.',
+    author='takatoh',
+    author_email='takatoh.m@gmail.com',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'click',
+        'PyYAML',
+        'requests'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+    ],
+    entry_points={
+        'console_scripts':[
+            'brs=brs.brs:main',
+        ],
+    }
+)
