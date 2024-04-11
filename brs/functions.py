888f8e8d2c1264e24408b05acd3d5ffb3e835f8f
takatoh <takatoh.m@gmail.com>
2024-04-11 14:08:03 +0900

    brs/functions.py(post_book()): Add 'bookshelf' field to post data.

--- brs/functions.py
+++ brs/functions.py
@@ -25,6 +25,7 @@ def post_book(data, uri_base):
         'note'           : data['note']           or '',
         'keyword'        : data['keyword']        or '',
         'disk'           : data['disc']           or '',
+        'bookshelf'      : data['disc']           or '',
         'disposed'       : '0'
     }
     uri = build_uri(uri_base, '/api/book/add/')
